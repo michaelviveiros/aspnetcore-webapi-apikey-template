8ca17c71b4c819e93163cb81d16d376b68b86641
Michael Viveiros <maykonviveiros@gmail.com>
2024-05-10 00:03:56 -0300

    feat: #3 - Feito implementação da chave base para posterior autenticação na API.

--- Source/TemplateWebApiKey.Api/Attributes/ApiKeyAttribute.cs
+++ Source/TemplateWebApiKey.Api/Attributes/ApiKeyAttribute.cs
@@ -5,6 +5,9 @@ namespace TemplateWebApiKey.Api.Attributes
     [AttributeUsage(validOn: AttributeTargets.Class | AttributeTargets.Method)]
     public class ApiKeyAttribute : Attribute, IAsyncActionFilter
     {
+        private const string ApiKeyName = "ApiSettings:ApiKeyName";
+        private const string ApiKeyValue = "ApiSettings:ApiKeyValue";
+
         public Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
             throw new NotImplementedException();
