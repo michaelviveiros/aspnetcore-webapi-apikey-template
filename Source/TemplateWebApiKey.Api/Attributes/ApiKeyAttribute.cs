ad3ee4b23775bd7536b5b7490e805eb4c6198c21
Michael Viveiros <maykonviveiros@gmail.com>
2024-05-10 19:08:56 -0300

    feat: #4 - Feito implementação da classe ApiKeyAttribute para filtrar a autenticação e autorização de acesso a todas as requisições de acesso a API.

--- Source/TemplateWebApiKey.Api/Attributes/ApiKeyAttribute.cs
+++ Source/TemplateWebApiKey.Api/Attributes/ApiKeyAttribute.cs
@@ -1,16 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace TemplateWebApiKey.Api.Attributes
 {
     [AttributeUsage(validOn: AttributeTargets.Class | AttributeTargets.Method)]
     public class ApiKeyAttribute : Attribute, IAsyncActionFilter
     {
-        private const string ApiKeyName = "ApiSettings:ApiKeyName";
+        private const string ApiKeyName = "api_key";
         private const string ApiKeyValue = "ApiSettings:ApiKeyValue";
 
-        public Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
-            throw new NotImplementedException();
+            if (!context.HttpContext.Request.Headers.TryGetValue(ApiKeyName, out var extractedApiKey))
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 401,
+                    Content = "Acesso não autorizado, chave de acesso não localizada, informe a chave para prosseguir."
+                };
+
+                return;
+            }
+
+            var appSettings = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+            var apiKey = appSettings.GetValue<string>(ApiKeyValue);
+
+            if (!apiKey.Equals(extractedApiKey))
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 403,
+                    Content = "Acesso não autorizado, a chave de acesso é inválida, revise os dados e tente novamente!"
+                };
+
+                return;
+            }
+
+            await next();
         }
     }
 }
